6f90d858e901464303f892e83a2f9b74ab1e16f2
block-doc-au <neilc@cns-gc.com.au>
2022-09-12 22:50:40 +1000

    NC Completed Arrays lab

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution here!
+const pets = ["Milo", "Otis", "Garfield"];
+const append = ["Milo", "Otis", "Garfield"];
+const prepend = ["Milo", "Otis", "Garfield"];
+const removeLast = ["Milo", "Otis", "Garfield"];
+const removeFirst = ["Milo", "Otis", "Garfield"];
+
+function toAppend(){
+      append.push("Odie");
+      return append;
+};
+
+toAppend();
+
+function toPrepend(){
+    prepend.unshift("Odie");
+    return prepend;
+};
+
+toPrepend();
+
+
+ function toRemoveLast(){
+    removeLast.pop();
+    return removeLast;
+};
+
+toRemoveLast();
+
+ function toRemoveFirst (){
+    removeFirst.shift();
+    return removeFirst;
+};
+
+toRemoveFirst();
+
